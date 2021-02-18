0a0c529f6958dfebc45b352f53c106555c6e17bf
jonathandiaz <jonathan.diaz714@gmail.com>
2021-02-18 03:05:08 -0500

    added todo list

--- Ghost/game.rb
+++ Ghost/game.rb
@@ -124,6 +124,7 @@ class Game
     end
 end
 
+# ask for the number of players and their names
 p1 = Player.new("Jonathan")
 p2 = Player.new("Ryan")
 p3 = Player.new("Rupesh")
@@ -132,8 +133,8 @@ GhostGame = Game.new(p1, p2, p3, p4)
 
 # Phase Bonus
 
-#     Write an AiPlayer class for your Ghost game. You'll need to figure out the logic for picking a winning letter on each turn. In order to do this, your AiPlayer will need to know both the current fragment and the number of other players (n).
-#         If adding a letter to the fragment would spell a word, then the letter is a losing move.
-#         If adding a letter to the fragment would leave only words with n or fewer additional letters as possibilities, then the letter is a winning move.
-#         Your AI should take any available winning move; if none is available, randomly select a losing move.
-#             See if you can improve your AI by computing the entire tree of possible moves from the current position. Choose the move that leaves the fewest losers and the most winners in the tree.
+# Write an AiPlayer class for your Ghost game. You'll need to figure out the logic for picking a winning letter on each turn. In order to do this, your AiPlayer will need to know both the current fragment and the number of other players (n).
+# If adding a letter to the fragment would spell a word, then the letter is a losing move.
+# If adding a letter to the fragment would leave only words with n or fewer additional letters as possibilities, then the letter is a winning move.
+# Your AI should take any available winning move; if none is available, randomly select a losing move.
+# See if you can improve your AI by computing the entire tree of possible moves from the current position. Choose the move that leaves the fewest losers and the most winners in the tree.
